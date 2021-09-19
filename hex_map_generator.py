5c9af75857bc0a090a3ee135555c63eff99b9536
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-09-19 12:50:45 -0400

    Added code to sketch hexagons, 6 vertices.

--- hex_map_generator.py
+++ hex_map_generator.py
@@ -8,3 +8,20 @@ grid_y_pixels = .9 * h
 
 sep_x = 3 * hex_size
 sep_y = .86 * hex_size
+
+grid_x = int(grid_x_pixels / sep_x) + 1
+grid_y = int(grid_y_pixels / sep_y) + 1
+
+def draw_hexagon(x, y, side):
+  beginShape()
+  vertex((x + side * sin(PI/2), y + side * cos(PI/2))) # vertex takes two params: (x, y)
+  vertex((x + side * sin(PI/6), y + side * cos(PI/6)))
+  vertex((x + side * sin(11 * PI/6), y + side * cos(11 * PI/6)))
+  vertex((x + side * sin(3 * PI/2), y + side * cos(3 * PI/2)))
+  vertex((x + side * sin(7 * PI/6), y + side * cos(7 * PI/6)))
+  vertex((x + side * sin(5 * PI/6), y + side * cos(5 * PI/6))) # 6 calls for six corners of hex (vertices; not sides)
+  endShape(CLOSE)
+
+# def setup():
+  # Create the Canvas
+  
