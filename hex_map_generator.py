211fddf824d5d43608c26463199487f78dc87e94
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-09-19 12:27:17 -0400

    Add 2 files. Initialize variables for hexagon dimensions.

---
+++ hex_map_generator.py
@@ -0,0 +1,10 @@
+# v.0.1 based on youtube; original was .pyde filetype; will convert to javascript v.0.2
+w, h = 1000, 1000
+
+hex_size = 10 # size in pixels, for canvas plugin
+
+grid_x_pixels = .9 * w
+grid_y_pixels = .9 * h
+
+sep_x = 3 * hex_size
+sep_y = .86 * hex_size
