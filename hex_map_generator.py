294d11ab814c6b9605edf9f87fce4dea0b421b28
Jeremy Abernathy <jea@JEREMYs-MacBook-Pro.local>
2021-09-19 12:58:50 -0400

    Complete v0.1 of Generator file.

--- hex_map_generator.py
+++ hex_map_generator.py
@@ -14,7 +14,7 @@ grid_y = int(grid_y_pixels / sep_y) + 1
 
 def draw_hexagon(x, y, side):
   beginShape()
-  vertex((x + side * sin(PI/2), y + side * cos(PI/2))) # vertex takes two params: (x, y)
+  vertex((x + side * sin(PI/2), y + side * cos(PI/2))) # vertex takes three params: (x, y, side)
   vertex((x + side * sin(PI/6), y + side * cos(PI/6)))
   vertex((x + side * sin(11 * PI/6), y + side * cos(11 * PI/6)))
   vertex((x + side * sin(3 * PI/2), y + side * cos(3 * PI/2)))
@@ -22,6 +22,29 @@ def draw_hexagon(x, y, side):
   vertex((x + side * sin(5 * PI/6), y + side * cos(5 * PI/6))) # 6 calls for six corners of hex (vertices; not sides)
   endShape(CLOSE)
 
-# def setup():
-  # Create the Canvas
-  
+def setup():
+  # Initialize canvas
+  size(w, h)
+  background(255) # bright white 
+
+  # Adjust resolution
+  pixelDensity(2)
+
+  # Shape details
+  strokeWeight(2)
+  stroke(0)
+  noFill()
+
+  # Draw grid
+  current_x = w/2.0 - grid_x_pixels/2.0
+  current_y = h/2.0 - grid_y_pixels/2.0
+  for i in range(grid_y):
+    if (i % 2 == 0):
+      current_x += 1.5 * hex_size
+    for j in range(grid_x):
+
+      draw_hexagon(current_x, current_y, hex_size)
+
+      current_x += sep_x
+    current_x = w/2.0 - grid_x_pixels/2.0
+    current_y += sep_y
